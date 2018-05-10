859981da7d7a94673816ed2145194fcbf5a09421
AHMED ABDULWAHAB <s338486830@ocdsb>
2018-05-10 11:05:10 -0400

    ook

---
+++ MainMenu(wip).py
@@ -0,0 +1,10 @@
+def mainMenu():
+
+       import pygame
+       pygame.init()
+
+       SCREENWIDTH =  900
+       SCREENHEIGHT = 850
+       size = (SCREENWIDTH, SCREENHEIGHT)
+       screen = pygame.display.set_mode(size)
+       
